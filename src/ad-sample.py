f6dc47c319b8f5546b0973385f7dcd16649ed24e
Susan Canelli <scanelli@device42.com>
2014-02-08 14:18:24 -0500

    Better OS handling (Specially for windows 8) and better error handling for non-responsive machines

--- src/ad-sample.py
+++ src/ad-sample.py
@@ -19,7 +19,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 #   to run:
 #       ipy.exe ad-sample.py
-#   v2, Updated: 09-12-2012
+#   v2.2, Updated: 02-08-2014
 ##############################################
 
 import types
@@ -40,7 +40,7 @@ from System.Management.Automation import RunspaceInvoke
 # create a runspace to run shell commands from
 RUNSPACE = RunspaceInvoke()
 
-BASE_URL='https://your-url-here'
+BASE_URL='https://your-url-here'        #make sure to NOT to end in /
 
 API_DEVICE_URL=BASE_URL+'/api/device/'
 API_IP_URL    =BASE_URL+'/api/ip/'
@@ -49,6 +49,7 @@ USER    ='put-your-user-name-here'
 PASSWORD='put-your-password-here'
 
 DRY_RUN = False # donot post just print the request that will be send
+DEBUG = True
 
 def post(url, params):
     """http post with basic-auth params is dict like object"""
@@ -64,21 +65,21 @@ def post(url, params):
         else:
             req = urllib2.Request(url, data, headers)
 
-            print '---REQUEST---',req.get_full_url()
-            print req.headers
-            print req.data
+            if DEBUG: print '---REQUEST---',req.get_full_url()
+            if DEBUG: print req.headers
+            if DEBUG: print req.data
 
             reponse = urllib2.urlopen(req)
 
-            print '---RESPONSE---'
-            print reponse.getcode()
-            print reponse.info()
-            print reponse.read()
+            if DEBUG: print '---RESPONSE---'
+            if DEBUG: print reponse.getcode()
+            if DEBUG: print reponse.info()
+            if DEBUG: print reponse.read()
     except urllib2.HTTPError as err:
         print '---RESPONSE---'
-        print err.getcode()
-        print err.info()
-        print err.read()
+        if DEBUG: print err.getcode()
+        if DEBUG: print err.info()
+        if DEBUG: print err.read()
     except urllib2.URLError as err:
         print '---RESPONSE---'
         print err
@@ -141,9 +142,13 @@ def to_ascii(s):
         return str(s)
         
 def closest_memory_assumption(v):
-    return int(256 * math.ceil(v / 256.0))
+    if v < 512: v = 128 * math.ceil(v / 128.0)
+    elif v < 1024: v = 256 * math.ceil(v / 256.0)
+    elif v < 4096: v = 512 * math.ceil(v / 512.0)
+    elif v < 8192: v = 1024 * math.ceil(v / 1024.0)
+    else: v = 2048 * math.ceil(v / 2048.0)
+    return int(v)
         
-
 def main():
     banner="""\
 
@@ -171,58 +176,61 @@ Which computer resources would you like in the report?
         print "ERROR: No computer found"
     else:
         for c in computers:
-            computer_system = wmi_1("Get-WmiObject Win32_ComputerSystem -Comp %s" % c)
-            operating_system = wmi_1("Get-WmiObject Win32_OperatingSystem -Comp %s" % c)
-            bios = wmi_1("Get-WmiObject Win32_BIOS -Comp %s" % c)
-            mem = closest_memory_assumption(int(computer_system.get('TotalPhysicalMemory')) / 1047552)
-            dev_name = to_ascii(computer_system.get('Name')).lower()
-            device = {
-                'name'          : dev_name,
-                'os'            : to_ascii(operating_system.get('Caption')),
-                'osver'         : to_ascii(operating_system.get('CSDVersion')),
-                'osmanufacturer': to_ascii(operating_system.get('Manufacturer')),
-                'osserial'      : to_ascii(operating_system.get('SerialNumber')),
-                'osverno'       : to_ascii(operating_system.get('Version')),
-                'memory'        : mem,
-            }
-            manufacturer = ''
-            for mftr in ['VMware, Inc.', 'Bochs', 'KVM', 'QEMU', 'Microsoft Corporation', 'Xen']:
-                if mftr == to_ascii(computer_system.get('Manufacturer')).strip():
-                    manufacturer = 'virtual'
-                    device.update({ 'manufacturer' : 'vmware', })
-                    break    
-            if manufacturer != 'virtual':
-                device.update({
-                    'manufacturer': to_ascii(computer_system.get('Manufacturer')).strip(),
-                    'hardware': to_ascii(computer_system.get('Model')).strip(),
-                    'serial_no': to_ascii(bios.get('SerialNumber')).strip(),
-                    })            
-            cpucount = 0
-            for cpu in wmi("Get-WmiObject Win32_Processor -Comp %s" % c):
-                cpucount += 1
-                cpuspeed = cpu.get('MaxClockSpeed')
-                cpucores = cpu.get('NumberOfCores')
-            if cpucount > 0:
-                
-                device.update({
-                    'cpucount': cpucount,
-                    'cpupower': cpuspeed,
-                    'cpucore':  cpucores,
-                    })
-            
-            
-            post(API_DEVICE_URL, device)
+            try:
+                computer_system = wmi_1("Get-WmiObject Win32_ComputerSystem -Comp %s" % c)
+                operating_system = wmi_1("Get-WmiObject Win32_OperatingSystem -Comp %s" % c)
+                bios = wmi_1("Get-WmiObject Win32_BIOS -Comp %s" % c)
+                mem = closest_memory_assumption(int(computer_system.get('TotalPhysicalMemory')) / 1047552)
+                dev_name = to_ascii(computer_system.get('Name')).lower()
+                device = {
+                    'name'          : dev_name,
+                    'memory'        : mem,
+                }
+                if 'Caption' in operating_system:
+                    device.update({'os' : to_ascii(operating_system.get('Caption'))})
+                    if 'CSDVersion' in operating_system: device.update({'osver' : to_ascii(operating_system.get('CSDVersion'))})
+                    if 'Manufacturer' in operating_system: device.update({'osmanufacturer': to_ascii(operating_system.get('Manufacturer'))})
+                    if 'SerialNumber' in operating_system: device.update({'osserial' : to_ascii(operating_system.get('SerialNumber'))})
+                    if 'Version' in operating_system: device.update({'osverno' : to_ascii(operating_system.get('Version'))})
+                manufacturer = ''
+                for mftr in ['VMware, Inc.', 'Bochs', 'KVM', 'QEMU', 'Microsoft Corporation', 'Xen']:
+                    if mftr == to_ascii(computer_system.get('Manufacturer')).strip():
+                        manufacturer = 'virtual'
+                        device.update({ 'manufacturer' : 'vmware', })
+                        break
+                if manufacturer != 'virtual':
+                    device.update({
+                        'manufacturer': to_ascii(computer_system.get('Manufacturer')).strip(),
+                        'hardware': to_ascii(computer_system.get('Model')).strip(),
+                        'serial_no': to_ascii(bios.get('SerialNumber')).strip(),
+                        })
+                cpucount = 0
+                for cpu in wmi("Get-WmiObject Win32_Processor -Comp %s" % c):
+                    cpucount += 1
+                    cpuspeed = cpu.get('MaxClockSpeed')
+                    cpucores = cpu.get('NumberOfCores')
+                if cpucount > 0:
 
-            for ntwk in wmi("Get-WmiObject Win32_NetworkAdapterConfiguration -Comp %s | where{$_.IPEnabled -eq \"True\"}" % c):
-                for ipaddr in ntwk.get('IPAddress'):
-                    ip = {
-                        'ipaddress'  : ipaddr,
-                        'macaddress' : ntwk.get('MACAddress'),
-                        'tag'        : ntwk.get('Description'),
-                        'device'     : dev_name,
-                    }
-                    try: post(API_IP_URL, ip)
-                    except: print 'Exception occured trying to upload info for IP: %s' % ipaddr
+                    device.update({
+                        'cpucount': cpucount,
+                        'cpupower': cpuspeed,
+                        'cpucore':  cpucores,
+                        })
 
+
+                post(API_DEVICE_URL, device)
+
+                for ntwk in wmi("Get-WmiObject Win32_NetworkAdapterConfiguration -Comp %s | where{$_.IPEnabled -eq \"True\"}" % c):
+                    for ipaddr in ntwk.get('IPAddress'):
+                        ip = {
+                            'ipaddress'  : ipaddr,
+                            'macaddress' : ntwk.get('MACAddress'),
+                            'tag'        : ntwk.get('Description'),
+                            'device'     : dev_name,
+                        }
+                        try: post(API_IP_URL, ip)
+                        except: print 'Exception occured trying to upload info for IP: %s' % ipaddr
+            except Exception, err:
+                print 'failed for machine', c, str(err)
 if __name__=="__main__":
     main()
